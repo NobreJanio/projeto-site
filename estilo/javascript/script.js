408bf04ddf9e86ce0acde32d550c584b4acc5981
NobreJanio <janionobrega@hotmail.com>
2021-11-16 21:24:44 -0300

    JS

---
+++ estilo/javascript/script.js
@@ -0,0 +1,12 @@
+//FizzBuzz
+//Divisivel por 3 => "Fizz"
+//Divisivel por 5 => "Buzz"
+//Divisivel por 3 e 5 => "FizzBuzz"
+//Se não for um número => "Não é um número"
+//se não for dividivel nem por 3 e nem por 5 => Entrada
+
+let resultado = fizzBuzz(15);
+console.log(resultado)
+
+function fizzBuzz(entrada) {
+}
